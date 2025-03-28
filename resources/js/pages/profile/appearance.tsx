73d506756698f3f45a26d475d10bdf4529cd5471
hakimihamzan <hakimihamzan20@gmail.com>
2025-03-28 19:10:32 +0800

    Add AppearanceTab component for theme selection in profile settings

---
+++ resources/js/pages/profile/appearance.tsx
@@ -0,0 +1,12 @@
+import AppearanceTab from '@/components/appearance-tab';
+import ProfileLayout from '@/layouts/app/profile-layout';
+
+function Appearance() {
+    return (
+        <ProfileLayout>
+            <AppearanceTab />
+        </ProfileLayout>
+    );
+}
+
+export default Appearance;
