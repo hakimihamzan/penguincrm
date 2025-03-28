e3a6f6128b080de6d92a263cfce6814b55a2191f
hakimihamzan <hakimihamzan20@gmail.com>
2025-03-28 21:43:22 +0800

    Enhance appearance page layout with themed card component

--- resources/js/pages/profile/appearance.tsx
+++ resources/js/pages/profile/appearance.tsx
@@ -1,10 +1,19 @@
 import AppearanceTab from '@/components/appearance-tab';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import ProfileLayout from '@/layouts/app/profile-layout';
 
 function Appearance() {
     return (
         <ProfileLayout>
-            <AppearanceTab />
+            <Card className="w-full max-w-2xl rounded">
+                <CardHeader>
+                    <CardTitle>Application Theme</CardTitle>
+                    <CardDescription>Update your prefered application theme.</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <AppearanceTab />
+                </CardContent>
+            </Card>
         </ProfileLayout>
     );
 }
