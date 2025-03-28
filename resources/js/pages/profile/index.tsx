b9aedc488dabf6cf63c5b3a49dd6c06152d2468a
hakimihamzan <hakimihamzan20@gmail.com>
2025-03-28 19:10:47 +0800

    Add skeleton Profile and Password pages with ProfileLayout component

---
+++ resources/js/pages/profile/index.tsx
@@ -0,0 +1,13 @@
+import ProfileLayout from '@/layouts/app/profile-layout';
+
+function Profile() {
+    return (
+        <ProfileLayout>
+            <div>
+                Profile
+            </div>
+        </ProfileLayout>
+    );
+}
+
+export default Profile;
---
+++ resources/js/pages/profile/password.tsx
@@ -0,0 +1,13 @@
+import ProfileLayout from '@/layouts/app/profile-layout';
+
+function Password() {
+    return (
+        <ProfileLayout>
+            <div>
+                Password
+            </div>
+        </ProfileLayout>
+    );
+}
+
+export default Password;
