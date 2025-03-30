a5cffb43f2258850a5aff1f66ca2aacc0830a1a8
hakimihamzan <hakimihamzan20@gmail.com>
2025-03-30 21:02:19 +0800

    Disable audio loading state and comment out iframe in AppSidebar component

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -27,7 +27,8 @@ const data = {
 };
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
-    const [isAudioLoading, setIsAudioLoading] = React.useState(true);
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [isAudioLoading, setIsAudioLoading] = React.useState(false);
 
     return (
         <Sidebar className="top-(--header-height) h-[calc(100svh-var(--header-height))]!" {...props}>
@@ -58,14 +59,14 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
                             <Loader2 className="text-muted-foreground h-6 w-6 animate-spin" />
                         </div>
                     )}
-                    <iframe
+                    {/* <iframe
                         width="100%"
                         height={125}
                         seamless
                         src="https://player.simplecast.com/fd0bd2ba-c553-466c-a060-b144797ce369?dark=false"
                         onLoad={() => setIsAudioLoading(false)}
                         style={{ opacity: isAudioLoading ? 0 : 1, transition: 'opacity 0.3s' }}
-                    />
+                    /> */}
                 </div>
                 <NavUser />
             </SidebarFooter>
