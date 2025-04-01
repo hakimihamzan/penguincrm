d42c49ad165413551d6e26a8f1fd01049a31c5ca
hakimihamzan <hakimihamzan20@gmail.com>
2025-04-01 23:37:32 +0800

    Add PaymentController and PaymentIndexRequest for managing payment listings

---
+++ app/Http/Controllers/PaymentController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Payment\PaymentIndexRequest;
+use App\Http\Resources\PaymentResource;
+use App\Models\Payment;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class PaymentController extends Controller
+{
+    /**
+     * Show the organizations list.
+     */
+    public function index(PaymentIndexRequest $request): Response
+    {
+        $validated = $request->validated();
+
+        $validated['sort_by'] = $validated['sort_by'] ?? 'id';
+        $validated['sort_order'] = $validated['sort_order'] ?? 'asc';
+
+        $validated = optional($validated);
+
+        $payments = Payment::query()
+            // sorting
+            ->when($validated['sort_by'], function ($query, $sortBy) use ($validated) {
+                return $query->orderBy($sortBy, $validated['sort_order'] ?? 'asc');
+            })
+            ->paginate($validated['per_page'] ?? 10, page: $validated['page'] ?? 1);
+
+        return Inertia::render('payment/index', [
+            'payments' => $payments->through(function ($payment) {
+                return (new PaymentResource($payment))->toArray(request());
+            })->values()->all(),
+            'pagination' => [
+                'current_page' => $payments->currentPage(),
+                'last_page' => $payments->lastPage(),
+                'per_page' => $payments->perPage(),
+                'total' => $payments->total(),
+            ],
+        ]);
+    }
+}
---
+++ app/Http/Requests/Payment/PaymentIndexRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\Payment;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
+
+class PaymentIndexRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return Auth::check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'sort_by' => ['nullable', 'string', Rule::in(['id', 'status', 'email', 'amount'])],
+            'sort_order' => ['nullable', 'string', Rule::in(['asc', 'desc'])],
+            'per_page' => ['nullable', 'integer', 'min:1'],
+            'page' => ['nullable', 'integer', 'min:1'],
+        ];
+    }
+}
