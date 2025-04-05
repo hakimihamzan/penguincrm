5990816733ddb00a3df279e1508bfa7c9284621c
hakimihamzan <hakimihamzan20@gmail.com>
2025-04-05 18:50:55 +0800

    feat: add updateAvatar method to UserController for handling avatar uploads

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\User\UserUpdateAvatarRequest;
 use App\Http\Requests\User\UserUpdateRequest;
+use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -15,8 +19,7 @@ class UserController extends Controller
      */
     public function index(Request $request): Response
     {
-        return Inertia::render('user', [
-        ]);
+        return Inertia::render('user', []);
     }
 
     /**
@@ -24,13 +27,38 @@ class UserController extends Controller
      */
     public function update(UserUpdateRequest $request): RedirectResponse
     {
-        $request->user()->fill($request->validated());
+        $input = $request->validated();
+
+        $request->user()->fill($input);
 
         if ($request->user()->isDirty('email')) {
             $request->user()->email_verified_at = null;
         }
 
         $request->user()->save();
+
+        return to_route('profile.me');
+    }
+
+    public function updateAvatar(UserUpdateAvatarRequest $request)
+    {
+        /** @var User */
+        $user = Auth::user();
+
+        $input = $request->validated();
+
+        $avatar = $input['avatar'];
+        $imageName = now()->format('Y_m_d_H_i_s') . '__' . $user->id  . '__' . $avatar->hashName();
+
+        $path = $avatar->storeAs('avatars', $imageName, 'public');
+
+        // delete the old image if it exists
+        if ($user->avatar) {
+            Storage::disk('public')->delete($user->avatar);
+        }
+
+        $user->avatar = $path;
+        $user->save();
 
         return to_route('profile.me');
     }
