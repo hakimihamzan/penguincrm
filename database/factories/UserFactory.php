6f3ee371eca20850b9d5ec6848b62d4f80a8a773
hakimihamzan <hakimihamzan20@gmail.com>
2025-04-05 15:26:52 +0800

    feat: add avatar field to User model and factory, update migration to include avatar column

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,12 +23,26 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $name = fake()->name();
+        $email = strtolower(str_replace(' ', '', $name)) . fake()->numberBetween(1, 1000) . '@' . fake()->freeEmailDomain();
+
+        $randomColor1 = fake()->randomElement(['008080', '0000FF', 'FF0000', '00FF00', 'FFFF00']);
+        $randomColor2 = fake()->randomElement(['008080', '0000FF', 'FF0000', '00FF00', 'FFFF00']);
+        $randomText = 'Hello+' . explode(' ', $name)[0];
+
+        if (fake()->boolean(50)) {
+            $avatar = "https://dummyjson.com/image/200x200/{$randomColor1}/{$randomColor2}?text={$randomText}";
+        } else {
+            $avatar = null;
+        }
+
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => $name,
+            'email' => $email,
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'avatar' => $avatar,
         ];
     }
 
@@ -37,7 +51,7 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
