d533e3c503c4227dd78b0a11566a4cbc2450524c
hakimihamzan <hakimihamzan20@gmail.com>
2025-04-06 01:52:04 +0800

    feat: implement DataTable component for organization data, WIP: sorting + filtering + searching

---
+++ resources/js/pages/organization/data-table.tsx
@@ -0,0 +1,108 @@
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
+import { ColumnDef, flexRender, getCoreRowModel, getSortedRowModel, useReactTable } from '@tanstack/react-table';
+import { useState } from 'react';
+import { Organization } from '.';
+
+interface DataTableProps<TData, TValue> {
+    columns: ColumnDef<TData, TValue>[];
+    organizations: Organization[];
+    pagination: {
+        current_page: number;
+        last_page: number;
+        per_page: number;
+        total: number;
+    };
+}
+
+export function DataTable<TData, TValue>({ columns, organizations, pagination }: DataTableProps<TData, TValue>) {
+    const [data, setData] = useState<TData[]>(organizations as TData[]);
+
+    const table = useReactTable({
+        data,
+        columns,
+        pageCount: pagination.last_page,
+        manualPagination: true,
+        manualSorting: true,
+        getCoreRowModel: getCoreRowModel(),
+        getSortedRowModel: getSortedRowModel(),
+    });
+
+    return (
+        <>
+            {pagination.total > 0 ? (
+                <div className="mt-8">
+                    <div className="flex items-center gap-2 py-4">
+                        <Input placeholder="Filter emails..." className="max-w-sm" />
+                    </div>
+                    <div className="rounded-md border">
+                        <Table>
+                            <TableHeader>
+                                {table.getHeaderGroups().map((headerGroup) => (
+                                    <TableRow key={headerGroup.id}>
+                                        {headerGroup.headers.map((header) => {
+                                            return (
+                                                <TableHead key={header.id}>
+                                                    {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
+                                                </TableHead>
+                                            );
+                                        })}
+                                    </TableRow>
+                                ))}
+                            </TableHeader>
+                            <TableBody>
+                                {table.getRowModel().rows?.length ? (
+                                    table.getRowModel().rows.map((row) => (
+                                        <TableRow key={row.id}>
+                                            {row.getVisibleCells().map((cell) => (
+                                                <TableCell key={cell.id} className="px-5">
+                                                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                                </TableCell>
+                                            ))}
+                                        </TableRow>
+                                    ))
+                                ) : (
+                                    <TableRow>
+                                        <TableCell colSpan={columns.length} className="h-24 text-center">
+                                            No results.
+                                        </TableCell>
+                                    </TableRow>
+                                )}
+                            </TableBody>
+                        </Table>
+                    </div>
+                    <div className="flex items-center justify-end space-x-2 py-4">
+                        <Button variant="outline" size="sm" onClick={() => table.previousPage()} disabled={!table.getCanPreviousPage()}>
+                            Previous
+                        </Button>
+                        <Button variant="outline" size="sm" onClick={() => table.nextPage()} disabled={!table.getCanNextPage()}>
+                            Next
+                        </Button>
+                    </div>
+                </div>
+            ) : (
+                <div className="mt-16 flex flex-col items-center justify-center border border-dashed py-12">
+                    <div className="bg-muted mb-4 rounded-full p-6">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="text-muted-foreground h-8 w-8"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth={2}
+                                d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4"
+                            />
+                        </svg>
+                    </div>
+                    <h3 className="mb-2 text-lg font-medium">No organizations found</h3>
+                    <p className="text-muted-foreground mb-4 text-center">There are no organization records in the system yet.</p>
+                </div>
+            )}
+        </>
+    );
+}
